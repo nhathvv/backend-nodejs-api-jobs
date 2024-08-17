658a80975ab9588e4634f243df6f75dc6ce6beba
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2024-08-17 15:40:07 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import databaseService from './services/database.service'
 import userRouter from './routes/users.routes'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
-app.use('/users',userRouter)
+app.use(cors())
+app.use('/users', userRouter)
 // Connect to MongoDB
 databaseService.connect()
 app.listen(port, () => {
